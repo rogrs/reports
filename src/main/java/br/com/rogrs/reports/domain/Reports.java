7e302380576cdf1925ba9bbb7ad13cc12986ecc4
Rogério Ribeiro <rogerio.rs@gmail.com>
2018-04-18 15:25:30 -0300

    Adicionado codigo da demanada

---
+++ src/main/java/br/com/rogrs/reports/domain/Reports.java
@@ -0,0 +1,55 @@
+package br.com.rogrs.reports.domain;
+
+
+import javax.persistence.Entity;
+
+import org.springframework.data.jpa.domain.AbstractPersistable;
+
+@Entity
+public class Reports extends AbstractPersistable<Long>{
+    
+    private static final long serialVersionUID = 1L;
+
+    private String jrxmlFilename;
+    
+    private String jasperFilename;
+    
+    private String pdfFilename;
+    
+    private String menuName;
+
+    private String teste;
+
+    public String getMenuName() {
+        return menuName;
+    }
+
+    public void setMenuName(String menuName) {
+        this.menuName = menuName;
+    }
+
+    public String getJrxmlFilename() {
+        return jrxmlFilename;
+    }
+
+    public void setJrxmlFilename(String jrxmlFilename) {
+        this.jrxmlFilename = jrxmlFilename;
+    }
+
+    public String getJasperFilename() {
+        return jasperFilename;
+    }
+
+    public void setJasperFilename(String jasperFilename) {
+        this.jasperFilename = jasperFilename;
+    }
+
+    public String getPdfFilename() {
+        return pdfFilename;
+    }
+
+    public void setPdfFilename(String pdfFilename) {
+        this.pdfFilename = pdfFilename;
+    }
+
+}
